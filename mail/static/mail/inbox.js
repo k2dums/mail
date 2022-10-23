1fc17f1d48613b5d46d085f2e27f4d3ed8998af0
k2dums <norbu.paddu@gmail.com>
2022-10-23 21:29:02 +0530

    function loadmail() changed to load_mail() The logic for the load_mail() was written
    Fetching of the data from the api route
    then adding div for sender,recipients,subject ...

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
-
+  console.log("I am composing an email")
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -52,6 +52,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view ').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   if (mailbox === "inbox")
   {
+    console.log("Fetching inbox mail");
     fetch('/emails/inbox')
     .then(response => response.json())
     .then(emails=>{
@@ -63,6 +64,7 @@ function load_mailbox(mailbox) {
 
 
   else if(mailbox==="sent"){
+    console.log("Fetching sent mail");
     fetch('/emails/sent')
     .then(response => response.json())
     .then(sent_emails => {
@@ -72,6 +74,7 @@ function load_mailbox(mailbox) {
   }
 
   else if (mailbox==="archive"){
+    console.log("Fetching archive mail");
     fetch('/emails/archive')
     .then(response => response.json())
     .then(archive_emails => {
@@ -82,19 +85,14 @@ function load_mailbox(mailbox) {
 
 }
 
-
-function loadmail(){
-console.log("An email was clicked");
-}
-
 function createEmailDiv(email){
   const wrapper=document.createElement("div");
   wrapper.classList.add('email_wrapper','row','border','border-dark','mb-2','p-1');
-
+  wrapper.addEventListener('click',()=>load_mail(email.id));
   const left_wrapper=document.createElement('div');
   left_wrapper.classList.add('left_wrapper','col');
-  const text_wrapper=document.createElement('div')
-  text_wrapper.classList.add('text_wrapper','row')
+  const text_wrapper=document.createElement('div');
+  text_wrapper.classList.add('text_wrapper','row');
   const sender=document.createElement('div');
   sender.classList.add('sender','col');
   const subject=document.createElement('div');
@@ -104,14 +102,73 @@ function createEmailDiv(email){
   const time=document.createElement('div','col');
   time.classList.add('time');
 
-  sender.innerHTML=`<b>${email.sender} <b>`
-  subject.innerHTML=email.subject
-  time.innerHTML=email.timestamp
+  sender.innerHTML=`<b>${email.sender} <b>`;
+  subject.innerHTML=email.subject;
+  time.innerHTML=email.timestamp;
 
   document.querySelector('#emails-view').append(wrapper);
-  left_wrapper.append(text_wrapper)
-  text_wrapper.append(sender)
-  text_wrapper.append(subject)
+  left_wrapper.append(text_wrapper);
+  text_wrapper.append(sender);
+  text_wrapper.append(subject);
   wrapper.append(left_wrapper);
   wrapper.append(time);
 }
+
+function load_mail(mail_id){
+  console.log(`Loading mail ${mail_id}`);
+  fetch(`emails/${mail_id}`)
+  .then(response=>response.json())
+  .then(email=>{
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  parent=document.querySelector('#emails-view');
+  parent.innerHTML=''
+
+  const mail_header=document.createElement('div');
+  mail_header.classList.add('mail_header','row','justify-content-end');
+  // const heading=document.createElement('h3');
+  // heading.innerHTML='Mail';
+  const status=document.createElement('button');
+  status.setAttribute('id','email_status');
+  if (email.archived==false){
+    status.classList.add('btn','btn-primary');
+    status.innerHTML='Archive';
+  }
+  else{
+    status.classList.add('btn','btn-dark');
+    status.innerHTML='UnArchive';
+  }
+  parent.append(mail_header);
+  // mail_header.append(heading);
+  mail_header.append(status)
+
+
+  const mail_container=document.createElement('div');
+  mail_container.classList.add('mail_container','container');
+  const mail_col_wrapper=document.createElement('div');
+  mail_col_wrapper.classList.add('mail_col_wrapper','col');
+
+  const subject=document.createElement('div');
+  const sender=document.createElement('div');
+  const body=document.createElement('div');
+  const recipients=document.createElement('div');
+  sender.classList.add('mail_sender');
+  subject.classList.add("mail_subject");
+  recipients.classList.add('mail_recipients');
+  body.classList.add('mail_body','mt-3');
+
+
+  subject.innerHTML=`<h4>Sub:${email.subject}</h4>`;
+  sender.innerHTML=`<b>From:</b> ${email.sender}`;
+  recipients.innerHTML=`<b>To:</b> ${email.recipients}`;
+  body.innerHTML=`${email.body}`
+  document.querySelector('#emails-view').append(mail_container);
+  mail_container.append(mail_col_wrapper)
+  mail_col_wrapper.append(subject);
+  mail_col_wrapper.append(sender);
+  mail_col_wrapper.append(recipients)
+  mail_col_wrapper.append(body);
+
+  });
+  }
